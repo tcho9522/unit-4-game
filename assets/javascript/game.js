b92bfdf7bdb6b7c4dc5a52ff78f62acca0aca625
Timothy Cho <tcho9522@gmail.com>
2018-10-27 22:34:00 -0500

    final push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,5 @@
-var charmander = {
-    name: "Charmander",
-    hp: 39,
-    attack: 52,
-    defense: 43,
-    speed: 65,
-    type: "fire",
-    move1: "Scratch",
-    move2: "Growl",     // Lowers attack
-    move3: "Ember",
-}
-var squirtle = {
-    name: "Squirtle",
-    hp: 44,
-    attack: 48,
-    defense: 65,        // Lowers defense
-    speed: 43,
-    type: "water",
-    move1: "Tackle",
-    move2: "Tailwhip",
-    move3: "Water gun",
-}
-var bulbasaur = {
-    name: "Bulbasaur",
-    hp: 45,
-    attack: 49,
-    defense: 49,
-    speed: 45,
-    type: "grass",
-    move1: "Tackle",
-    move2: "Mud shot",     // Lowers speed
-    move3: "Razor leaf",
-}
-
-
+var currentOpp = ["onix", "starmie", "raichu", "vileplume", "weezing", "alakazam", "arcanine", "nidoking"];
+var oppIndex = 0;
 var chosenPokemon;
 var starter;
 $(".pokePics").on("click", function () {
@@ -57,11 +24,15 @@ $(".pokePics").on("click", function () {
         $(".portraitBox").html("<img id='portrait' src='assets/images/bulbasaur.png' alt=''>")
     }
     $(".dialogue").text("You chose " + starter.name + "!");
-    $(".button1").html(starter.move1).click(function() {
-        console.log("you used " + starter.move1)
+    $(".button1").html(starter.moves[0]).click(function () {
+        console.log("you used " + starter.moves[0])
     });
-    $(".button2").html(starter.move2);
-    $(".button3").html(starter.move3);
+    $(".button2").html(starter.moves[1]).click(function () {
+        console.log("you used " + starter.moves[1]);
+    });
+    $(".button3").html(starter.moves[2]).click(function () {
+        console.log("you used " + starter.moves[2]);
+    });
     $(".button4").html("---");
     initializeFight(starter.name);
     $(".pokePics").off("click");        // So that you can only choose once
@@ -75,52 +46,86 @@ function Pokemon(name, hp, attack, defense, speed, type, move1, move2, move3) {
     this.defense = defense;
     this.speed = speed;
     this.type = type;
-    this.move1 = move1;
-    this.move2 = move2;
-    this.move3 = move3;
+    this.moves = [move1, move2, move3]
+
 }
-function initializeFight(starter){
-    if(starter==="Charmander"){
+function initializeFight(starter) {
+    var opponent;
+    if (starter === "Charmander") {
         $(".pokePortrait").html("<img id = 'portrait' src ='assets/images/charmander.png' alt=''>")
-        $("#onix").css('filter', 'blur(0px) grayscale(0%)');
-        
+    }
+    else if (starter === "Squirtle") {
+        $(".pokePortrait").html("<img id = 'portrait' src ='assets/images/squirtle.png' alt=''>")
+    }
+    else if (starter === "Bulbasaur") {
+        $(".pokePortrait").html("<img id = 'portrait' src ='assets/images/bulbasaur.png' alt=''>")
+    }
+    switch (currentOpp[oppIndex]) {
+        case "onix":
+            opponent = new Pokemon("Onix", 50, 50, 50, 50, "rock", "Tackle", "Tailwhip", "Rock throw");
+            $("#onix").css('filter', 'blur(0px) grayscale(0%)');
+            break;
+        case "starmie":
+
+            break;
+        case "raichu":
+
+            break;
+        case "vileplume":
+
+            break;
+        case "weezing":
+
+            break;
+        case "alakazam":
+
+            break;
+        case "arcanine":
+
+            break;
+        case "nidoking":
+
+            break;
+    }
+    promptMove(opponent);
+
+}
+
+
+function promptMove(opponent) {
+    console.log("initiateMove function executed");
+    // var opponent = new Pokemon(eijraewoijroaewijraeroeuhrwaeiurhaewuira)
+    // Initialize p1 stats
+    var p1=starter;
+    var p1hp = starter.hp
+    var p1Attack = starter.attack;
+    var p1Defense = starter.defense;
+    var p1Speed = starter.speed;
+
+    // Initialize p2 stats
+    var p2 = opponent
+    var p2hp = opponent.hp;
+    var p2Attack = opponent.attack;
+    var p2Defense = opponent.defense;
+    var p2Speed = opponent.speed;
+
+    console.log(p1hp + " " + p2hp);
+    while (p1hp > 0 && p2hp > 0) {
+        // Determine who goes first
+        if (p1Speed > p2Speed) {
+            var first = p1;
+            var second = p2;
+            console.log(first.name + "'s speed is " + first.speed + ". " + second.name + "'s speed is " + second.speed);
+            console.log(p1.name + " is faster");
+        }
+        else {
+            var first = p2;
+            var second = p1;
+        }
+        //
+        second.hp = second.hp - (first.attack - second.defense)
+        console.log(first.name + " attacked " + second.name + ". Attack did " + first.attack + ". second");
+        break;
     }
 }
-
-// function initiateFight(p1, p2) {
-//     console.log("initiateMove function executed");
-//     console.log(p1);
-
-//     // Initialize p1 stats
-//     var p1hp = p1.hp
-//     var p1Attack = p1.attack;
-//     var p1Defense = p1.defense;
-//     var p1Speed = p1.speed;
-
-//     // Initialize p2 stats
-//     var p2hp = p2.hp;
-//     var p2Attack = p2.attack;
-//     var p2Defense = p2.defense;
-//     var p2Speed = p2.speed;
-
-//     console.log(p1hp + " " + p2hp);
-//     while (p1hp > 0 && p2hp > 0) {
-//         // Determine who goes first
-//         if (p1Speed > p2Speed) {
-//             var first = p1;
-//             var second = p2;
-//             console.log(first.name + "'s speed is " + first.speed + ". " + second.name + "'s speed is " + second.speed);
-//             console.log(p1.name + " is faster");
-//         }
-//         else {
-//             var first = p2;
-//             var second = p1;
-//         }
-//         //
-//         second.hp = second.hp - (first.attack - second.defense)
-//         console.log(first.name + " attacked " + second.name + ". Attack did " + first.attack + ". second");
-//         break;
-//     }
-// }
-
-// initiateFight(charmander, bulbasaur);
+// ITERATE THE currentOpp array!!!!!
